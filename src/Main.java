f76f04905006a86a133cac76ceb84f5e266a8d30
Mohamed Shamekh <shamekhjr@gmail.com>
2023-05-08 19:22:23 +0300

    Added Main file

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Main {
+
+    public void fetch() {
+
+    }
+
+    public void decode(){
+
+    }
+
+    public void execute() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
