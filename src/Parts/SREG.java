bca9832d3de28cf3f39efc01f76f4b9b9c08b186
Mohamed Shamekh <shamekhjr@gmail.com>
2023-05-09 13:03:11 +0300

    Added class for Status Register (methods still need to be implemented)

---
+++ src/Parts/SREG.java
@@ -0,0 +1,61 @@
+package src.Parts;
+
+public class SREG {
+    private byte value = 0b00000000;
+
+    //decide on params later
+    //set Carry Flag (C)
+    public void setCarryFlag() {
+
+    }
+
+    //set Two's Complement Overflow Flag (V)
+    public void setOverflowFlag() {
+
+    }
+
+    //set Sign Flag (S)
+    public void setSignFlag() {
+
+    }
+
+    //set Negative Flag (N)
+    public void setNegativeFlag() {
+
+    }
+
+    //set Zero Flag (Z)
+    public void setZeroFlag() {
+
+    }
+
+    //get Carry Flag (C)
+    public byte getCarryFlag() {
+        return 0;
+    }
+
+    //get Two's Complement Overflow Flag (V)
+    public byte getOverflowFlag() {
+        return 0;
+    }
+
+    //get Negative Flag (N)
+    public byte getNegativeFlag() {
+        return 0;
+    }
+
+    //get Zero Flag (Z)
+    public byte getZeroFlag() {
+        return 0;
+    }
+
+    //get Sign Flag (S)
+    public byte getSignFlag() {
+        return 0;
+    }
+
+
+
+
+
+}
