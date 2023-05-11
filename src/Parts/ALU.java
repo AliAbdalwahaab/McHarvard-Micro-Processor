aeb8048292dc2628e85c3406bc43e1d70fa7f522
Mohamed Shamekh <shamekhjr@gmail.com>
2023-05-11 20:14:00 +0300

    Created ALU class

---
+++ src/Parts/ALU.java
@@ -0,0 +1,13 @@
+package src.Parts;
+
+public class ALU {
+    public SREG sreg;
+    private byte operand1;
+    private byte operand2;
+    private byte result;
+    private short opcode;
+
+
+
+
+}
