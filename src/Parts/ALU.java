ef60c237b6a0dec325b77725364d6bd6b12a7ade
Mohamed Shamekh <shamekhjr@gmail.com>
2023-05-11 20:15:06 +0300

    ALU class updates

--- src/Parts/ALU.java
+++ src/Parts/ALU.java
@@ -4,10 +4,8 @@ public class ALU {
     public SREG sreg;
     private byte operand1;
     private byte operand2;
-    private byte result;
+    private short result;
     private short opcode;
-
-
 
 
 }
