109b9dcff3e29ab002a6613325ce505e275e286e
Mohamed Shamekh <shamekhjr@gmail.com>
2023-05-11 20:52:09 +0300

    Added constructor for ALU class

--- src/Parts/ALU.java
+++ src/Parts/ALU.java
@@ -7,5 +7,22 @@ public class ALU {
     private short result;
     private byte opcode;
 
+    public ALU(byte operand1, byte operand2, byte opcode) {
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+        this.opcode = opcode;
+        sreg = new SREG();
+    }
+
+    public short getResult() {
+        //TODO: implement the logic for the ALU
+        return result;
+    }
+
+    //will be used to compute new value of pc in case of branch or jr
+    public short ALUAdder(short pc, byte imm) {
+        return 0;
+    }
+
 
 }
