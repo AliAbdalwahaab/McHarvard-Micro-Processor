19ec8f3e5c2c3ad09dc2cf43262a8e13caf7b218
Mohamed Shamekh <shamekhjr@gmail.com>
2023-05-17 00:05:11 +0300

    Started implementing parser

--- src/Processor.java
+++ src/Processor.java
@@ -1,23 +1,78 @@
 package src;
 
+import src.Parts.ALU;
+import src.Parts.DataMemory;
+import src.Parts.InstructionMemory;
+import src.Parts.RegisterFile;
+
+import java.io.*;
+
 public class Processor {
-    int cycles = 1;
-    int pc = 0;
+    static int cycles = 1;
+    static short pc = 0;
+    static ALU alu = new ALU();
+    static RegisterFile regFile = new RegisterFile();
+    static DataMemory dataMem = new DataMemory();
+    static InstructionMemory instructionMem = new InstructionMemory();
+
 
     public static void fetch() {
 
     }
 
-    public static void decode(){
+    public static void decode() {
 
     }
 
     public static void execute() {
+        //TODO: we need to remember to handle the case that if a branch succeeds we need to handle aborting the instructions in the IF and ID stages
+
+    }
+
+    public void parse(String fileName) {
+        File file=new File(fileName);    //creates a new file instance
+        FileReader fr= null;   //reads the file
+
+        try {
+            fr = new FileReader(file);
+            BufferedReader br=new BufferedReader(fr);  //creates a buffering character input stream
+            String line;
+            while((line=br.readLine())!=null)
+            {
+                String[] splitted = line.split(" ");
+                String instruction = "";
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
 
     }
 
     public static void main(String[] args) {
-        //TODO: we need to remember to handle the case that if a branch succeeds (we find out in the EX stage) we need to handle aborting the instructions in the IF and ID stages
+
+
+        //parse the .txt file
+
+        while (instructionMem.getInstruction(pc) != 0) {
+
+            short oldPc = pc;
+
+            short instruction = instructionMem.getInstruction(pc);
+            pc++;
+
+            if (oldPc == 0) { //first instruction so I will only fetch cycle
+
+            } else { //i will fetch, decode, execute then print cycle
+
+            }
+
+
+        }
         //System.out.println("Hello World!");
     }
 }
